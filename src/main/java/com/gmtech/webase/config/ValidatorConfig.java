40dc2170eb0f798f61ae0bd9395a8f029e10491f
zhengjintao <xiaonei0912@qq.com>
2020-05-04 23:06:01 +0900

    custom validator message resource forld

--- src/main/java/com/gmtech/webase/config/ValidatorConfig.java
+++ src/main/java/com/gmtech/webase/config/ValidatorConfig.java
@@ -1,29 +1,36 @@
 package com.gmtech.webase.config;
 
-import javax.annotation.Resource;
-
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.validation.Validator;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
 @Configuration
-public class ValidatorConfig {
-	/**
-     * MessageSource
-     */
-    @Resource
-    private MessageSource messageSource;
+public class ValidatorConfig extends WebMvcConfigurationSupport{
+	
+	@Bean
+	public MessageSource getMessageSource() {
+		ResourceBundleMessageSource messageSource=new ResourceBundleMessageSource();
+    	messageSource.setBasename("ValidationMessages/ValidationMessages");
+    	return messageSource;
+	}
+	
+	@Override
+    public Validator getValidator() {
+        return validator();
+    }
 
     /**
      * Validation message i18n
      * @return Validator
      */
     @Bean
-    public Validator getValidator() {
+    public Validator validator() {
         LocalValidatorFactoryBean validator = new LocalValidatorFactoryBean();
-        validator.setValidationMessageSource(this.messageSource);
+        validator.setValidationMessageSource(getMessageSource());
         return validator;
     }
 }
